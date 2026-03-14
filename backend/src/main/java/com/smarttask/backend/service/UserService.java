d69d6590148c61ac1d15d9ff56b5f203aedd67e7
KanirshaMehra <kanirshamehra@gmail.com>
2026-03-15 00:07:14 +0530

    Added BCryptPasswordEncoder

--- backend/src/main/java/com/smarttask/backend/service/UserService.java
+++ backend/src/main/java/com/smarttask/backend/service/UserService.java
@@ -1,20 +1,26 @@
 package com.smarttask.backend.service;
 
+import com.smarttask.backend.config.SecurityConfig;
 import com.smarttask.backend.dto.UserResponse;
 import com.smarttask.backend.entity.User;
 import com.smarttask.backend.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, SecurityConfig securityConfig, BCryptPasswordEncoder bCryptPasswordEncoder, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponse registerUser(User user){
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         User savedUser=userRepository.save(user);
 
